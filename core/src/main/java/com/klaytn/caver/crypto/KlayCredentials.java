5ec31917f178e023dd632ec0d4a9546a0b05fae4
rick <rick.no@groundx.xyz>
2019-10-15 13:12:16 +0900

    add ecKeyPairList for transaction, update, fee payer.

--- core/src/main/java/com/klaytn/caver/crypto/KlayCredentials.java
+++ core/src/main/java/com/klaytn/caver/crypto/KlayCredentials.java
@@ -24,20 +24,57 @@ import org.web3j.crypto.ECKeyPair;
 import org.web3j.crypto.Keys;
 import org.web3j.utils.Numeric;
 
+import java.util.*;
+
 import static com.klaytn.caver.wallet.KlayWalletUtils.*;
 
 public class KlayCredentials {
-
-    private final ECKeyPair ecKeyPair;
+    private final List<ECKeyPair> ecKeyPairForTransactionList;
+    private final List<ECKeyPair> ecKeyPairForUpdateList;
+    private final List<ECKeyPair> ecKeyPairForFeeFeePayerList;
     private final String address;
 
     private KlayCredentials(ECKeyPair ecKeyPair, String address) {
-        this.ecKeyPair = ecKeyPair;
+        this.ecKeyPairForTransactionList = Collections.unmodifiableList(Arrays.asList(ecKeyPair));
+        this.ecKeyPairForUpdateList = null;
+        this.ecKeyPairForFeeFeePayerList = null;
+        this.address = address;
+    }
+
+    private KlayCredentials(List<ECKeyPair> ecKeyPairForTransactionArray, List<ECKeyPair> ecKeyPairForUpdateArray, List<ECKeyPair> ecKeyPairForFeeArray, String address) {
+        this.ecKeyPairForTransactionList = (ecKeyPairForTransactionArray != null && ecKeyPairForTransactionArray.size() != 0) ? Collections.unmodifiableList(ecKeyPairForTransactionArray) : null;
+        this.ecKeyPairForUpdateList = (ecKeyPairForUpdateArray != null && ecKeyPairForUpdateArray.size() != 0) ? Collections.unmodifiableList(ecKeyPairForUpdateArray) : null;
+        this.ecKeyPairForFeeFeePayerList = (ecKeyPairForFeeArray != null && ecKeyPairForFeeArray.size() != 0) ? Collections.unmodifiableList(ecKeyPairForFeeArray) : null;
         this.address = address;
     }
 
     public ECKeyPair getEcKeyPair() {
-        return ecKeyPair;
+        return ecKeyPairForTransactionList.get(0);
+    }
+
+    public List<ECKeyPair> getEcKeyPairsForTransactionList() throws NullPointerException {
+        if (ecKeyPairForTransactionList != null) {
+            return ecKeyPairForTransactionList;
+        }
+        throw new RuntimeException("Transaction key does not exist.");
+    }
+
+    public List<ECKeyPair> getEcKeyPairsForUpdateList() throws NullPointerException {
+        if (ecKeyPairForUpdateList != null) {
+            return ecKeyPairForUpdateList;
+        } else if (ecKeyPairForTransactionList != null) {
+            return ecKeyPairForTransactionList;
+        }
+        throw new RuntimeException("Update key does not exist.");
+    }
+
+    public List<ECKeyPair> getEcKeyPairsForFeePayerList() throws NullPointerException {
+        if (ecKeyPairForFeeFeePayerList != null) {
+            return ecKeyPairForFeeFeePayerList;
+        } else if (ecKeyPairForTransactionList != null) {
+            return ecKeyPairForTransactionList;
+        }
+        throw new RuntimeException("Fee key does not exist.");
     }
 
     public String getAddress() {
@@ -61,6 +98,52 @@ public class KlayCredentials {
 
     public static KlayCredentials create(ECKeyPair ecKeyPair, String address) {
         return new KlayCredentials(ecKeyPair, address);
+    }
+
+    public static KlayCredentials create(List<ECKeyPair> ecKeyPair, String address) {
+        return new KlayCredentials(ecKeyPair, null, null, address);
+    }
+
+    public static KlayCredentials create(List<ECKeyPair> ecKeyPairForTransaction, List<ECKeyPair> ecKeyPairForUpdate, List<ECKeyPair> ecKeyPairForFeePayer, String address) {
+        return new KlayCredentials(ecKeyPairForTransaction, ecKeyPairForUpdate, ecKeyPairForFeePayer, address);
+    }
+
+    public static KlayCredentials create(ECKeyPair[] ecKeyPairsArrayForTransaction, ECKeyPair[] ecKeyPairsArrayForUpdate, ECKeyPair[] ecKeyPairsArrayForFee, String address) {
+        List<ECKeyPair> ecKeyPairsForTransaction = new ArrayList<>(Arrays.asList(ecKeyPairsArrayForTransaction));
+        List<ECKeyPair> ecKeyPairsForUpdate = new ArrayList<>(Arrays.asList(ecKeyPairsArrayForUpdate));
+        List<ECKeyPair> ecKeyPairsForForFee = new ArrayList<>(Arrays.asList(ecKeyPairsArrayForFee));
+
+        return create(ecKeyPairsForTransaction, ecKeyPairsForUpdate, ecKeyPairsForForFee, address);
+    }
+
+    public static KlayCredentials create(String[] privateKeysForTransaction, String[] privateKeysForUpdate, String[] privateKeysForFee, String address) {
+        List<ECKeyPair> ecKeyPairsForTransaction = new ArrayList<>();
+        List<ECKeyPair> ecKeyPairsForUpdate = new ArrayList<>();
+        List<ECKeyPair> ecKeyPairsForForFee = new ArrayList<>();
+
+        for (String privateKey : privateKeysForTransaction) {
+            ecKeyPairsForTransaction.add(ECKeyPair.create(Numeric.toBigInt(privateKey)));
+        }
+
+        for (String privateKey : privateKeysForUpdate) {
+            ecKeyPairsForUpdate.add(ECKeyPair.create(Numeric.toBigInt(privateKey)));
+        }
+
+        for (String privateKey : privateKeysForFee) {
+            ecKeyPairsForForFee.add(ECKeyPair.create(Numeric.toBigInt(privateKey)));
+        }
+
+        return create(ecKeyPairsForTransaction, ecKeyPairsForUpdate, ecKeyPairsForForFee, address);
+    }
+
+    public static KlayCredentials create(String[] privateKeysForTransaction, String address) {
+        List<ECKeyPair> ecKeyPairsForTransaction = new ArrayList<>();
+
+        for (String privateKey : privateKeysForTransaction) {
+            ecKeyPairsForTransaction.add(ECKeyPair.create(Numeric.toBigInt(privateKey)));
+        }
+
+        return create(ecKeyPairsForTransaction, null, null, address);
     }
 
     public static KlayCredentials createWithKlaytnWalletKey(String klaytnWalletKey) {
@@ -87,17 +170,34 @@ public class KlayCredentials {
 
         KlayCredentials that = (KlayCredentials) o;
 
-        if (ecKeyPair != null ? !ecKeyPair.equals(that.ecKeyPair) : that.ecKeyPair != null) {
+        if (!Objects.equals(ecKeyPairForTransactionList, that.ecKeyPairForTransactionList)) {
             return false;
         }
 
-        return address != null ? address.equals(that.address) : that.address == null;
+        if (!Objects.equals(ecKeyPairForUpdateList, that.ecKeyPairForUpdateList)) {
+            return false;
+        }
+
+        if (!Objects.equals(ecKeyPairForFeeFeePayerList, that.ecKeyPairForFeeFeePayerList)) {
+            return false;
+        }
+
+        return Objects.equals(address, that.address);
     }
 
     @Override
     public int hashCode() {
-        int result = ecKeyPair != null ? ecKeyPair.hashCode() : 0;
-        result = 31 * result + (address != null ? address.hashCode() : 0);
+        int transactionHashCode = ecKeyPairForTransactionList != null ? ecKeyPairForTransactionList.hashCode() : 0;
+        int updateHashCode = ecKeyPairForUpdateList != null ? ecKeyPairForUpdateList.hashCode() : 0;
+        int feeHashCode = ecKeyPairForUpdateList != null ? ecKeyPairForUpdateList.hashCode() : 0;
+        int addressHashCode = address != null ? address.hashCode() : 0;
+
+        int[] arrHashCode = {feeHashCode, updateHashCode, addressHashCode, transactionHashCode};
+        int result = 0;
+        for (int hashCode : arrHashCode) {
+            result = result * 31 + hashCode;
+        }
+
         return result;
     }
 }

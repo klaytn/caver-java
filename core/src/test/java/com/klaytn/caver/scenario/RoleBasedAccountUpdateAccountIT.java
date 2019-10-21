1c83fc4c07e57378f46c1e2a56ea0c258561079e
rick <rick.no@groundx.xyz>
2019-10-21 18:24:47 +0900

    reflect review

--- core/src/test/java/com/klaytn/caver/scenario/RoleBasedAccountUpdateAccountIT.java
+++ core/src/test/java/com/klaytn/caver/scenario/RoleBasedAccountUpdateAccountIT.java
@@ -1,11 +1,14 @@
 package com.klaytn.caver.scenario;
 
 import com.klaytn.caver.crypto.KlayCredentials;
+import com.klaytn.caver.methods.response.KlayAccountKey;
 import com.klaytn.caver.methods.response.KlayTransactionReceipt;
+import com.klaytn.caver.tx.account.AccountKey;
 import com.klaytn.caver.tx.model.AccountUpdateTransaction;
 import com.klaytn.caver.tx.model.TransactionTransformer;
-import com.klaytn.caver.tx.type.TxTypeFeeDelegate;
+import com.klaytn.caver.tx.type.*;
 import org.junit.Test;
+import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.utils.Numeric;
 
 import java.math.BigInteger;
@@ -17,6 +20,13 @@ public class RoleBasedAccountUpdateAccountIT extends RoleBasedAccountScenario {
 
     private final ReceiptChecker getAccountUpdateReceiptChecker = (TransactionTransformer transactionTransformer, KlayTransactionReceipt.TransactionReceipt transactionReceipt) -> {
         assertEquals("0x1", transactionReceipt.getStatus());
+
+        AccountKey newAccountKey = getNewAccountKey(transactionTransformer);
+
+        KlayAccountKey klayAccountKey = caver.klay().getAccountKey(transactionReceipt.getFrom(), DefaultBlockParameterName.LATEST).send();
+        AccountKey responseAccountKey = klayAccountKey.getResult().getKey();
+
+        assertEquals("Response\n" + responseAccountKey.toString() + "\nExpected" + newAccountKey.toString(), responseAccountKey, newAccountKey);
     };
 
     private final ReceiptChecker getFeeRatioAccountUpdateReceiptChecker = (TransactionTransformer transactionTransformer, KlayTransactionReceipt.TransactionReceipt transactionReceipt) -> {
@@ -24,11 +34,17 @@ public class RoleBasedAccountUpdateAccountIT extends RoleBasedAccountScenario {
 
         TxTypeFeeDelegate txTypeFeeDelegate = (TxTypeFeeDelegate) transactionTransformer.build();
         assertEquals(Numeric.toHexStringWithPrefix(txTypeFeeDelegate.getFeeRatio()), transactionReceipt.getFeeRatio());
+
+        AccountKey newAccountKey = getNewAccountKey(transactionTransformer);
+
+        KlayAccountKey klayAccountKey = caver.klay().getAccountKey(transactionReceipt.getFrom(), DefaultBlockParameterName.LATEST).send();
+        AccountKey responseAccountKey = klayAccountKey.getResult().getKey();
+
+        assertEquals("Response\n" + responseAccountKey.toString() + "\nExpected" + newAccountKey.toString(), responseAccountKey, newAccountKey);
     };
 
     private final AccountUpdateTransaction getAccountUpdateTransactionTransformer(String from) throws Exception {
         KlayCredentials oldAccount = createAccount();
-
         RoleBasedAccountGenerator roleBasedAccountGenerator = new RoleBasedAccountGenerator();
         roleBasedAccountGenerator.setRandomRoleBasedNewAccountKey(oldAccount, 10, 10, 10);
 
@@ -113,5 +129,23 @@ public class RoleBasedAccountUpdateAccountIT extends RoleBasedAccountScenario {
         feeDelegatedRoleBasedTransactionMultiTransactionSignerMultiFeePayerTest(
                 (String from) -> getAccountUpdateTransactionTransformer(from).feeDelegate().feeRatio(feeRatio),
                 getFeeRatioAccountUpdateReceiptChecker, true);
+    }
+
+    private AccountKey getNewAccountKey(TransactionTransformer transactionTransformer) throws Exception {
+        AccountKey newAccountKey = null;
+        TxType txType = transactionTransformer.build();
+
+        switch (txType.getType()) {
+            case ACCOUNT_UPDATE:
+                newAccountKey = ((TxTypeAccountUpdate) txType).getAccountKey();
+                break;
+            case FEE_DELEGATED_ACCOUNT_UPDATE:
+                newAccountKey = ((TxTypeFeeDelegatedAccountUpdate) txType).getAccountKey();
+                break;
+            case FEE_DELEGATED_ACCOUNT_UPDATE_WITH_RATIO:
+                newAccountKey = ((TxTypeFeeDelegatedAccountUpdateWithRatio) txType).getAccountKey();
+                break;
+        }
+        return newAccountKey;
     }
 }

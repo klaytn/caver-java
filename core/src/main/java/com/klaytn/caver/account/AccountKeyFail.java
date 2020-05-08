0c9ffb379dd9ddf558daee9c63523f9d8048dfb2
kale.kim <kale.kim@groundx.xyz>
2020-05-08 10:33:21 +0900

    add class - AccountKeyFail, AccountKeyLegacy

---
+++ core/src/main/java/com/klaytn/caver/account/AccountKeyFail.java
@@ -0,0 +1,54 @@
+package com.klaytn.caver.account;
+
+import org.web3j.utils.Numeric;
+
+import java.util.Arrays;
+
+/**
+ * AccountKeyFail is used for smart contract accounts so that a transaction sent from
+ * the smart contract account always fails.
+ * If an account has the key AccountKeyFail, the tx validation process always fails.
+ */
+public class AccountKeyFail implements IAccountKey{
+    private static final byte[] RLP = new byte[]{(byte) 0x03, (byte) 0xc0};
+    private static final byte TYPE = 0x03;
+
+
+    /**
+     * Decodes a RLP-encoded AccountKeyFail string
+     * @param rlpEncodedKey RLP-encoded AccountKeyPublic string
+     * @return AccountKeyFail
+     */
+    public static AccountKeyFail decode(String rlpEncodedKey) {
+        return decode(Numeric.hexStringToByteArray(rlpEncodedKey));
+    }
+
+    /**
+     * Decodes a RLP-encoded AccountKeyFail byte array
+     * @param rlpEncodedKey RLP-encoded AccountKeyFail byte array
+     * @return AccountKeyFail
+     */
+    public static AccountKeyFail decode(byte[] rlpEncodedKey) {
+        if(!Arrays.equals(rlpEncodedKey, RLP)) {
+            throw new RuntimeException("Invalid RLP-encoded account key String");
+        }
+
+        return new AccountKeyFail();
+    }
+
+    protected AccountKeyFail() {
+    }
+
+    /**
+     * Encodes a AccountKeyPublic Object by RLP-Encoding method.
+     * @return RLP-encoded AccountKeyPublic String
+     */
+    @Override
+    public String getRLPEncoding() {
+        return Numeric.toHexString(RLP);
+    }
+
+    public static byte getType() {
+        return TYPE;
+    }
+}
---
+++ core/src/main/java/com/klaytn/caver/account/AccountKeyLegacy.java
@@ -0,0 +1,64 @@
+package com.klaytn.caver.account;
+
+import com.klaytn.caver.tx.account.AccountKey;
+import org.web3j.utils.Numeric;
+
+import java.util.Arrays;
+
+/**
+ * AccountKeyLegacy represents a key of legacy account types. If an account has AccountKeyLegacy,
+ * the tx validation process is done like below (as Ethereum did):
+ * <ul>
+ * <li> Get the public key from ecrecover(txhash, txsig) </li>
+ * <li> Get the address of the public key </li>
+ * <li> The address is the sender </li>
+ * </ul>
+ */
+public class AccountKeyLegacy implements IAccountKey{
+
+    private static final byte[] RLP = new byte[]{(byte) 0x01, (byte) 0xc0};
+    private static final byte TYPE = 0x01;
+
+
+    /**
+     * Decodes a RLP-encoded AccountKeyLegacy string
+     * @param rlpEncodedKey A RLP-encoded AccountKeyLegacy string
+     * @return AccountKeyLegacy
+     */
+    static AccountKeyLegacy decode(String rlpEncodedKey) {
+        return decode(Numeric.hexStringToByteArray(rlpEncodedKey));
+    }
+
+    /**
+     * Decodes a RLP-encoded AccountKeyLegacy byte array
+     * @param rlpEncodedKey RLP-encoded AccountKeyLegacy byte array
+     * @return AccountKeyLegacy
+     */
+    static AccountKeyLegacy decode(byte[] rlpEncodedKey) {
+        if(!Arrays.equals(RLP, rlpEncodedKey)) {
+            throw new RuntimeException("Invalid RLP-encoded account key String");
+        }
+
+        return new AccountKeyLegacy();
+    }
+
+    /**
+     * Encodes a AccountKeyLegacy Object by RLP-encoding method.
+     * @return RLP-encoded AccountKeyLegacy string
+     */
+
+    @Override
+    public String getRLPEncoding() {
+        return Numeric.toHexString(RLP);
+    }
+
+
+
+    protected AccountKeyLegacy() {
+
+    }
+
+    public static byte getType() {
+        return TYPE;
+    }
+}

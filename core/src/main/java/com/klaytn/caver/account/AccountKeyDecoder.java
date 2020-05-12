e5a9eaf0d8410e4da2f59dbb3720b0e0eb3aefcd
kale.kim <kale.kim@groundx.xyz>
2020-05-13 00:20:43 +0900

    add AccountKeyRoleBased class and AccountKeyDecoder

---
+++ core/src/main/java/com/klaytn/caver/account/AccountKeyDecoder.java
@@ -0,0 +1,32 @@
+package com.klaytn.caver.account;
+
+import org.web3j.utils.Numeric;
+
+/**
+ * Representing an AccountKeyDecoder which can decode RLP-encoded accountKey string.
+ */
+public class AccountKeyDecoder {
+
+    /**
+     * Decodes a RLP-encoded AccountKey implements IAccountKey interface
+     * @param rlpEncodedKey RLP-encoded AccountKey string.
+     * @return AccountKey implements IAccountKey interface
+     */
+    public static IAccountKey decode(String rlpEncodedKey) {
+        String hexPrefixEncoded = Numeric.prependHexPrefix(rlpEncodedKey);
+
+        if(hexPrefixEncoded.startsWith(AccountKeyFail.getType())) {
+            return AccountKeyFail.decode(hexPrefixEncoded);
+        } else if(hexPrefixEncoded.startsWith(AccountKeyLegacy.getType())) {
+            return AccountKeyLegacy.decode(hexPrefixEncoded);
+        } else if(hexPrefixEncoded.startsWith(AccountKeyPublic.getType())) {
+            return AccountKeyPublic.decode(hexPrefixEncoded);
+        } else if(hexPrefixEncoded.startsWith(AccountKeyWeightedMultiSig.getType())) {
+            return AccountKeyWeightedMultiSig.decode(hexPrefixEncoded);
+        } else if(hexPrefixEncoded.startsWith(AccountKeyRoleBased.getType())) {
+            return AccountKeyRoleBased.decode(hexPrefixEncoded);
+        } else {
+            throw new RuntimeException("Invalid RLP-encoded account key string");
+        }
+    }
+}
---
+++ core/src/main/java/com/klaytn/caver/account/AccountKeyRoleBased.java
@@ -0,0 +1,209 @@
+package com.klaytn.caver.account;
+
+import com.klaytn.caver.utils.BytesUtils;
+import org.web3j.rlp.*;
+import org.web3j.utils.Numeric;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * AccountKeyRoleBased represents a role-based key.
+ */
+public class AccountKeyRoleBased implements IAccountKey{
+
+    /**
+     * AccountKeyRoleBased's Type attribute.
+     */
+    private static final String TYPE = "0x05";
+
+    public static final String RLP_ACCOUNT_NIL = "0x80";
+
+    public static final int MAX_ROLE_BASED_KEY_COUNT = 3;
+    public static final int OFFSET_TRANSACTION_ROLE = 0;
+    public static final int OFFSET_ACCOUNT_UPDATE_ROLE = 1;
+    public static final int OFFSET_FEEPAYER_ROLE = 2;
+
+    /**
+     * First Key : roleTransaction
+     * Default key. Transactions other than TxTypeAccountUpdate should be signed by the key of this role.
+     *
+     * Second Key : roleUpdate
+     * TxTypeAccountUpdate transaction should be signed by this key. If this key is not present in the account,
+     * TxTypeAccountUpdate transaction is validated using RoleTransaction key.
+     *
+     * Third Key : roleFeePayer
+     * If this account wants to send tx fee instead of the sender, the transaction should be signed by this key.
+     * If this key is not present in the account, a fee-delegated transaction is validated using RoleTransaction key.
+     */
+    private List<IAccountKey> accountKeys;
+
+    /**
+     * Creates an AccountKeyRoleBased instance.
+     * @param accountKeys List of a AccountKey implements IAccountKey interface
+     */
+    public AccountKeyRoleBased(List<IAccountKey> accountKeys) {
+        setAccountKeys(accountKeys);
+    }
+
+    /**
+     * Decodes a RLP-encoded AccountKeyRoleBased string
+     * @param rlpEncodedKey RLP-encoded AccountKeyRoleBased string.
+     * @return AccountKeyRoleBased
+     */
+    public static AccountKeyRoleBased decode(String rlpEncodedKey) {
+        return decode(Numeric.hexStringToByteArray(rlpEncodedKey));
+    }
+
+    /**
+     * Decodes a RLP-encoded AccountKeyRoleBased byte array
+     * @param rlpEncodedKey RLP-encoded AccountKeyRoleBased byte array
+     * @return AccountKeyRoleBased
+     */
+    public static AccountKeyRoleBased decode(byte[] rlpEncodedKey) {
+        byte type = Numeric.hexStringToByteArray(getType())[0];
+        if(rlpEncodedKey[0] != type) {
+            throw new IllegalArgumentException("Invalid RLP-encoded AccountKeyRoleBased Tag");
+        }
+
+        List<IAccountKey> accountKeys = new ArrayList<>();
+        //remove Tag
+        byte[] encodedKey = Arrays.copyOfRange(rlpEncodedKey, 1, rlpEncodedKey.length);
+
+        RlpList rlpList = RlpDecoder.decode(encodedKey);
+        List<RlpType> values = ((RlpList) rlpList.getValues().get(0)).getValues();
+
+        for (RlpType value : values) {
+            accountKeys.add(AccountKeyDecoder.decode(((RlpString) value).asString()));
+        }
+        return new AccountKeyRoleBased(accountKeys);
+    }
+
+    /**
+     * Creates an AccountKeyRoleBased give params.
+     * @param pubArray An List contains public key string array.
+     * @param options An List contains WeightedMultiSigOptions
+     * @return AccountKeyRoleBased
+     */
+    public static AccountKeyRoleBased fromRoleBasedPublicKeysAndOptions(List<String[]> pubArray, List<WeightedMultiSigOptions> options) {
+        List<IAccountKey> accountKeys = new ArrayList<>();
+
+        /*
+        * pubArray must have three item.
+        * each item can have
+        * {String, String...}, {}
+        *
+        * options must have to three item.
+        * Valid WeightedMultiSigOption or Empty WeightedMultiSigOption
+        *  - Valid WeightedMultiSigOption : make AccountKeyWeightedMultiSig instance
+        *  - Empty WeightedMultiSigOption : make AccountKeyPublicKey instance
+        * */
+        if(pubArray.size() != 3 && options.size() != 3) {
+            throw new RuntimeException("");
+        }
+
+        for(int i=0; i<pubArray.size(); i++) {
+            String[] publicKeyArr = pubArray.get(i);
+            WeightedMultiSigOptions weightedMultiSigOption = options.get(i);
+            if(publicKeyArr == null) {
+                throw new RuntimeException("Invalid publicKey format");
+            }
+
+            //Set AccountKeyNil
+            if(publicKeyArr.length == 0) {
+                if (options.get(i) != null) {
+                    throw new RuntimeException("Invalid options: AccountKeyNil cannot have options.");
+                }
+                accountKeys.add(null);
+                continue;
+            }
+            //Set AccountKeyPublic
+            else if (publicKeyArr.length == 1) {
+                if (weightedMultiSigOption.isEmpty()) {
+                    accountKeys.add(AccountKeyPublic.fromPublicKey(publicKeyArr[0]));
+                    continue;
+                }
+            }
+
+            if (weightedMultiSigOption == null) {
+                throw new NullPointerException("Invalid options : AccountKeyWeightedMultiSig must have options");
+            }
+            accountKeys.add(AccountKeyWeightedMultiSig.fromPublicKeysAndOptions(publicKeyArr, weightedMultiSigOption));
+        }
+
+        return new AccountKeyRoleBased(accountKeys);
+    }
+
+    /**
+     * Getter function for accountKeys
+     * @return accountKeys
+     */
+    public List<IAccountKey> getAccountKeys() {
+        return accountKeys;
+    }
+
+    /**
+     * Setter function for accountKeys
+     * @param accountKeys List of a AccountKey implements IAccountKey interface
+     */
+    public void setAccountKeys(List<IAccountKey> accountKeys) {
+        if(accountKeys.size() > MAX_ROLE_BASED_KEY_COUNT) throw new RuntimeException("It exceeds maximum role based key count.");
+        this.accountKeys = accountKeys;
+    }
+
+    /**
+     * Encodes a AccountKeyRoleBased Object by RLP-encoding method.
+     * @return RLP-encoded AccountKeyRoleBased String
+     */
+    @Override
+    public String getRLPEncoding() {
+        List<RlpType> rlpTypeList = new ArrayList<>();
+        for(IAccountKey accountKey: accountKeys) {
+            byte[] encodedData;
+            if (accountKey == null) {
+                encodedData = Numeric.hexStringToByteArray(RLP_ACCOUNT_NIL);
+            } else {
+                encodedData = Numeric.hexStringToByteArray(accountKey.getRLPEncoding());
+            }
+
+            rlpTypeList.add(RlpString.create(encodedData));
+        }
+        byte[] encodedRoleBasedKey = RlpEncoder.encode(new RlpList(rlpTypeList));
+        byte[] type = Numeric.hexStringToByteArray(AccountKeyRoleBased.getType());
+
+        return Numeric.toHexString(BytesUtils.concat(type, encodedRoleBasedKey));
+    }
+
+    /**
+     * Returns a AccountKeyRoleBased's type attribute
+     * @return AccountKeyRoleBased's type attribute
+     */
+    public static String getType() {
+        return TYPE;
+    }
+
+    /**
+     * Returns a TransactionRole key
+     * @return IAccountKey
+     */
+    public IAccountKey getTransactionKey() {
+        return this.getAccountKeys().get(OFFSET_TRANSACTION_ROLE);
+    }
+
+    /**
+     * Returns a AccountUpdate key
+     * @return IAccountKey
+     */
+    public IAccountKey getAccountUpdateKey() {
+        return this.getAccountKeys().get(OFFSET_ACCOUNT_UPDATE_ROLE);
+    }
+
+    /**
+     * Returns a FeePayer key
+     * @return IAccountKey
+     */
+    public IAccountKey getFeePayerKey() {
+        return this.getAccountKeys().get(OFFSET_FEEPAYER_ROLE);
+    }
+}

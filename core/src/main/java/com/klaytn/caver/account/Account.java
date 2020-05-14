ee604cc0ac3c1457277380c7ad5416d4f74c9055
kale.kim <kale.kim@groundx.xyz>
2020-05-14 10:43:58 +0900

    add Account class

---
+++ core/src/main/java/com/klaytn/caver/account/Account.java
@@ -0,0 +1,220 @@
+package com.klaytn.caver.account;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Representing an Account which includes information for account update
+ */
+public class Account {
+    /**
+     * The address of Account
+     */
+    private String address;
+
+    /**
+     * The AccountKey of Account
+     */
+    private IAccountKey accountKey;
+
+    /**
+     * Creates an Account instance
+     * @param address The address of Account
+     * @param accountKey The accountKey of Account
+     */
+    public Account(String address, IAccountKey accountKey) {
+        this.address = address;
+        this.accountKey = accountKey;
+    }
+
+    /**
+     * Create an Account instance that contains the AccountKeyPublic instance
+     * @param address The address of Account
+     * @param publicKey public key
+     * @return Account
+     */
+    public static Account create(String address, String publicKey) {
+        return createWithAccountKeyPublic(address, publicKey);
+    }
+                                                 
+    /**
+     * Create an Account instance that contains the AccountKeyWeightedMultiSig instance
+     * This method set 1 to WeightedMultiSigOptions values(threshold, weights)
+     * @param address address of Account
+     * @param publicKeys public key array
+     * @return Account
+     */
+    public static Account create(String address, String[] publicKeys) {
+        return createWithAccountKeyWeightedMultiSig(address, publicKeys);
+    }
+
+    /**
+     * Create an Account instance that contains AccountKeyWeightedMultiSig instance
+     * @param address address of Account
+     * @param publicKeys public key array
+     * @param options WeightedMultiSigOptions
+     * @return Account
+     */
+    public static Account create(String address, String[] publicKeys, WeightedMultiSigOptions options) {
+        return createWithAccountKeyWeightedMultiSig(address, publicKeys, options);
+    }
+
+    /**
+     * Create an Account instance that contains AccountKeyRoleBased instance
+     * This method set 1 to WeightedMultiSigOptions values(threshold, weights)
+     * @param address address of Account
+     * @param publicKeyList List of public key array
+     * @return Account
+     */
+    public static Account create(String address, List<String[]> publicKeyList) {
+        return createWithAccountKeyRoleBased(address, publicKeyList);
+    }
+
+    /**
+     * Create an Account instance that contains AccountKeyRoleBased instance
+     * @param address address of Account
+     * @param publicKeyList List of public key array
+     * @param optionsList List of WeightedMultiSigOptions
+     * @return Account
+     */
+    public static Account create(String address, List<String[]> publicKeyList, List<WeightedMultiSigOptions> optionsList ) {
+        return createWithAccountKeyRoleBased(address, publicKeyList, optionsList);
+    }
+
+    /**
+     * Create an Account instance from RLP-encoded account key
+     * @param address address of Account
+     * @param rlpEncodedKey RLP-encoded account key string
+     * @return Account
+     */
+    public static Account createFromRLPEncoding(String address, String rlpEncodedKey) {
+        IAccountKey accountKey = AccountKeyDecoder.decode(rlpEncodedKey);
+        return new Account(address, accountKey);
+    }
+
+    /**
+     * Create an Account instance which has AccountKeyLegacy as an accountKey
+     * @param address address of Account
+     * @return Account
+     */
+    public static Account createWithAccountKeyLegacy(String address) {
+        IAccountKey accountKey = new AccountKeyLegacy();
+        return new Account(address, accountKey);
+    }
+
+    /**
+     * Create an Account instance which has AccountKeyFail as an accountKey
+     * @param address address of Account
+     * @return Account
+     */
+    public static Account createWithAccountKeyFail(String address) {
+        IAccountKey accountKey = new AccountKeyFail();
+        return new Account(address, accountKey);
+    }
+
+    /**
+     * Create an Account instance which has AccountKeyPublic as an accountKey
+     * @param address address of Account
+     * @return Account
+     */
+    public static Account createWithAccountKeyPublic(String address, String publicKey) {
+        IAccountKey accountKey = AccountKeyPublic.fromPublicKey(publicKey);
+        return new Account(address, accountKey);
+    }
+
+    /**
+     * Create an Account instance which has AccountKeyWeightedMultiSig as an accountKey
+     * This method set 1 to WeightedMultiSigOptions values(threshold, weights)
+     * @param address address of Account
+     * @return Account
+     */
+    public static Account createWithAccountKeyWeightedMultiSig(String address, String[] publicKeys) {
+        WeightedMultiSigOptions options = getDefaultOptions(publicKeys.length);
+        return createWithAccountKeyWeightedMultiSig(address, publicKeys, options);
+    }
+
+    /**
+     * Create an Account instance which has AccountKeyWeightedMultiSig as an accountKey
+     * @param address address of Account
+     * @param publicKeys List of public key array
+     * @param options List of WeightedMultiSigOptions
+     * @return Account
+     */
+    public static Account createWithAccountKeyWeightedMultiSig(String address, String[] publicKeys, WeightedMultiSigOptions options) {
+        IAccountKey accountKey = AccountKeyWeightedMultiSig.fromPublicKeysAndOptions(publicKeys, options);
+        return new Account(address, accountKey);
+    }
+
+    /**
+     * Create an Account instance which has AccountKeyRoleBased as an accountKey
+     * This method set 1 to WeightedMultiSigOptions values(threshold, weights)
+     * @param address address of Account
+     * @param roleBasedPublicKey List of public key array
+     * @return Account
+     */
+    public static Account createWithAccountKeyRoleBased(String address, List<String[]> roleBasedPublicKey) {
+        List<WeightedMultiSigOptions> optionList = new ArrayList<>();
+
+        for(int i=0; i<roleBasedPublicKey.size(); i++) {
+            WeightedMultiSigOptions option = getDefaultOptions(roleBasedPublicKey.get(i).length);
+            optionList.add(option);
+        }
+
+        return createWithAccountKeyRoleBased(address, roleBasedPublicKey, optionList);
+    }
+
+    /**
+     * Create an Account instance which has AccountKeyRoleBased as an accountKey
+     * @param address address of Account
+     * @param roleBasedPublicKey List of public key array
+     * @param optionsList List of WeightedMultiSigOptions
+     * @return Account
+     */
+    public static Account createWithAccountKeyRoleBased(String address, List<String[]> roleBasedPublicKey, List<WeightedMultiSigOptions> optionsList) {
+        IAccountKey accountKey = AccountKeyRoleBased.fromRoleBasedPublicKeysAndOptions(roleBasedPublicKey, optionsList);
+
+        return new Account(address, accountKey);
+    }
+
+    /**
+     * Set 1 to WeightedMultiSigOptions values(threshold, weights)
+     * @param arrayLength Length of public key array
+     * @return WeightedMultiSigOptions
+     */
+    private static WeightedMultiSigOptions getDefaultOptions(int arrayLength) {
+        BigInteger threshold = BigInteger.ONE;
+        BigInteger[] weightArr = new BigInteger[arrayLength];
+
+        for(BigInteger element : weightArr) {
+            element = BigInteger.ONE;
+        }
+
+        return new WeightedMultiSigOptions(threshold, Arrays.asList(weightArr));
+    }
+
+    /**
+     * Returns RLP-encoded accountKey string
+     * @returnf String
+     */
+    public String getRLPEncodingAccountKey() {
+        return this.getAccountKey().getRLPEncoding();
+    }
+
+    /**
+     * Getter function for address
+     * @return address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Getter function for accountKey
+     * @return accountKey
+     */
+    public IAccountKey getAccountKey() {
+        return accountKey;
+    }
+}

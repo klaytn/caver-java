04c15c503bb3c80ad9706c7f5b05411d0af1019f
kale.kim <kale.kim@groundx.xyz>
2020-06-02 11:07:21 +0900

    add TransactionDecoder class

---
+++ core/src/main/java/com/klaytn/caver/transaction/TransactionDecoder.java
@@ -0,0 +1,17 @@
+package com.klaytn.caver.transaction;
+
+import com.klaytn.caver.transaction.type.TransactionType;
+import org.web3j.utils.Numeric;
+
+public class TransactionDecoder {
+
+    public static AbstractTransaction decode(String rlpEncoded) {
+        byte[] rlpBytes = Numeric.hexStringToByteArray(rlpEncoded);
+
+        if(rlpBytes[0] == TransactionType.TxTypeValueTransfer.getType()) {
+            return null;
+        } else {
+            return LegacyTransaction.decode(rlpBytes);
+        }
+    }
+}

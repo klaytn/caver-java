a76417e1ad8ba850a145f88111a0a38c8626516d
kale.kim <kale.kim@groundx.xyz>
2020-06-25 16:52:08 +0900

    modify test code

--- core/src/test/java/com/klaytn/caver/base/Accounts.java
+++ core/src/test/java/com/klaytn/caver/base/Accounts.java
@@ -33,28 +33,36 @@ import static com.klaytn.caver.base.LocalValues.LOCAL_CHAIN_ID;
 
 public class Accounts {
 
-    public static final KlayCredentials LUMAN = KlayCredentials.create(
-            "0x2359d1ae7317c01532a58b01452476b796a3ac713336e97d8d3c9651cc0aecc3",
-            "0x2c8ad0ea2e0781db8b8c9242e07de3a5beabb71a"
-    );
+    public static KlayCredentials LUMAN;
 
-    public static final KlayCredentials WAYNE = KlayCredentials.create(
-            "0x92c0815f28b20cc22fff5fcf41adc80efe9d7ebe00439628b468f2f88a0aadc4",
-            "0x3cd93ba290712e6d28ac98f2b820faf799ae8fdb"
-    );
+    public static KlayCredentials WAYNE;
 
-    public static final KlayCredentials BRANDON = KlayCredentials.create(
-            "0x734aa75ef35fd4420eea2965900e90040b8b9f9f7484219b1a06d06394330f4e",
-            "0xe97f27e9a5765ce36a7b919b1cb6004c7209217e"
-    );
+    public static KlayCredentials BRANDON;
 
-    public static final KlayCredentials FEE_PAYER = KlayCredentials.create(
-            "0x1e558ea00698990d875cb69d3c8f9a234fe8eab5c6bd898488d851669289e178",
-            "0x9d0dcbe163be73163348e7f96accb2b9e1e9dcf6"
-    );
+    public static KlayCredentials FEE_PAYER;
 
     @BeforeClass
     public static void fillUpKlayToAccount() {
+        LUMAN = KlayCredentials.create(
+                "0x2359d1ae7317c01532a58b01452476b796a3ac713336e97d8d3c9651cc0aecc3",
+                "0x2c8ad0ea2e0781db8b8c9242e07de3a5beabb71a"
+        );
+
+        WAYNE = KlayCredentials.create(
+                "0x92c0815f28b20cc22fff5fcf41adc80efe9d7ebe00439628b468f2f88a0aadc4",
+                "0x3cd93ba290712e6d28ac98f2b820faf799ae8fdb"
+        );
+
+        BRANDON = KlayCredentials.create(
+                "0x734aa75ef35fd4420eea2965900e90040b8b9f9f7484219b1a06d06394330f4e",
+                "0xe97f27e9a5765ce36a7b919b1cb6004c7209217e"
+        );
+
+        FEE_PAYER = KlayCredentials.create(
+                "0x1e558ea00698990d875cb69d3c8f9a234fe8eab5c6bd898488d851669289e178",
+                "0x9d0dcbe163be73163348e7f96accb2b9e1e9dcf6"
+        );
+
         List<KlayCredentials> testCredentials = new ArrayList<>(Arrays.asList(LUMAN, WAYNE, BRANDON, FEE_PAYER));
         fillUpKlay(testCredentials);
     }

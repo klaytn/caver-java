fa21764012bcf404d8ce784cc21f07990ab0c7c0
kale.kim <kale.kim@groundx.xyz>
2020-06-30 11:50:20 +0900

    remove not supported JSON-RPC API test

--- core/src/test/java/com/klaytn/caver/feature/RpcTest.java
+++ core/src/test/java/com/klaytn/caver/feature/RpcTest.java
@@ -303,23 +303,6 @@ public class RpcTest {
     }
 
     @Test
-    public void testGetWork() throws Exception {
-        Response<List<String>> response = caver.klay().getWork().send();
-        List<String> result = response.getResult();
-        assertEquals(3, result.size());
-        assertNotNull(result.get(0));
-        assertNotNull(result.get(1));
-        assertNotNull(result.get(2));
-    }
-
-    @Test
-    public void testIsMining() throws Exception {
-        Boolean result = caver.klay().isMining().send();
-        boolean isMining = result.getResult();
-        assertTrue(isMining);
-    }
-
-    @Test
     public void testIsSyncing() throws Exception {
         KlaySyncing response = caver.klay().isSyncing().send();
         KlaySyncing.Result result = response.getResult();

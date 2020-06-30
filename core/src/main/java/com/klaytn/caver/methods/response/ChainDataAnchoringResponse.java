cede931c9e0b498208619c7887581468e0dc5582
kale.kim <kale.kim@groundx.xyz>
2020-06-30 10:24:04 +0900

    add decodeChainDataAnchoring DTO Object

---
+++ core/src/main/java/com/klaytn/caver/methods/response/ChainDataAnchoringResponse.java
@@ -0,0 +1,111 @@
+package com.klaytn.caver.methods.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.web3j.protocol.core.Response;
+
+public class ChainDataAnchoringResponse extends Response<ChainDataAnchoringResponse.ChainDataAnchoringData> {
+
+    public static class ChainDataAnchoringData {
+        @JsonProperty("TxHash")
+        private String txHash;
+
+        @JsonProperty("TxCount")
+        private int txCount;
+
+        @JsonProperty("StateRootHash")
+        private String stateRootHash;
+
+        @JsonProperty("ReceiptHash")
+        private String receiptHash;
+
+        @JsonProperty("ParentHash")
+        private String parentHash;
+
+        @JsonProperty("BlockNumber")
+        private int blockNumber;
+
+        @JsonProperty("BlockHash")
+        private String blockHash;
+
+        @JsonProperty("BlockCount")
+        private int blockCount;
+
+        public ChainDataAnchoringData() {
+        }
+
+        public ChainDataAnchoringData(String txHash, int txCount, String stateRootHash, String receiptHash, String parentHash, int blockNumber, String blockHash, int blockCount) {
+            this.txHash = txHash;
+            this.txCount = txCount;
+            this.stateRootHash = stateRootHash;
+            this.receiptHash = receiptHash;
+            this.parentHash = parentHash;
+            this.blockNumber = blockNumber;
+            this.blockHash = blockHash;
+            this.blockCount = blockCount;
+        }
+
+        public String getTxHash() {
+            return txHash;
+        }
+
+        public void setTxHash(String txHash) {
+            this.txHash = txHash;
+        }
+
+        public int getTxCount() {
+            return txCount;
+        }
+
+        public void setTxCount(int txCount) {
+            this.txCount = txCount;
+        }
+
+        public String getStateRootHash() {
+            return stateRootHash;
+        }
+
+        public void setStateRootHash(String stateRootHash) {
+            this.stateRootHash = stateRootHash;
+        }
+
+        public String getReceiptHash() {
+            return receiptHash;
+        }
+
+        public void setReceiptHash(String receiptHash) {
+            this.receiptHash = receiptHash;
+        }
+
+        public String getParentHash() {
+            return parentHash;
+        }
+
+        public void setParentHash(String parentHash) {
+            this.parentHash = parentHash;
+        }
+
+        public int getBlockNumber() {
+            return blockNumber;
+        }
+
+        public void setBlockNumber(int blockNumber) {
+            this.blockNumber = blockNumber;
+        }
+
+        public String getBlockHash() {
+            return blockHash;
+        }
+
+        public void setBlockHash(String blockHash) {
+            this.blockHash = blockHash;
+        }
+
+        public int getBlockCount() {
+            return blockCount;
+        }
+
+        public void setBlockCount(int blockCount) {
+            this.blockCount = blockCount;
+        }
+    }
+}

caec0063070f9832aed9d16bdf7c09367b5ad83d
kale.kim <kale.kim@groundx.xyz>
2020-07-30 14:05:39 +0900

    cleanup code

--- core/src/test/java/com/klaytn/caver/common/ContractTest.java
+++ core/src/test/java/com/klaytn/caver/common/ContractTest.java
@@ -981,8 +981,7 @@ public class ContractTest {
                 BRANDON.getAddress(),
                 amount
         );
-
-        SendOptions sendOptions = new SendOptions(LUMAN.getAddress(), DefaultGasProvider.GAS_LIMIT);
+        
         String gas = contract.getMethod("transfer").estimateGas(sendParams, CallObject.createCallObject());
 
         assertEquals("0x5a58", gas);

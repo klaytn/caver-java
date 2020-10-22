76c55ac52e394ff2a8a4d1f020b54ea85ef7f1c9
kale.kim <kale.kim@groundx.xyz>
2020-10-22 16:17:02 +0900

    modify test

--- core/src/test/java/com/klaytn/caver/ipfs/IPFSTest.java
+++ core/src/test/java/com/klaytn/caver/ipfs/IPFSTest.java
@@ -3,12 +3,33 @@ package com.klaytn.caver.ipfs;
 import org.junit.Test;
 import org.web3j.utils.Numeric;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class IPFSTest {
+
+    static String text = "This is IPFS test.";
+    static String fileName = "./ipfs.txt";
+
+    public void createFile() {
+
+
+        File file = new File(fileName);
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(file, true);
+            fw.write(text);
+            fw.flush();
+
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void fromHex() {
@@ -21,28 +42,27 @@ public class IPFSTest {
     @Test
     public void toHex() {
         String encodedMultiHash = "QmYtUc4iTCbbfVSDNKvtQqrfyezPPnFvE33wFmutw9PBBk";
-        String expectedMultiHashData = "12209cbc07c3f991725836a3aa2a581ca2029198aa420b9d99bc0e131d9f3e2cbe47";
+        String expectedMultiHashData = "0x12209cbc07c3f991725836a3aa2a581ca2029198aa420b9d99bc0e131d9f3e2cbe47";
 
         assertEquals(IPFS.toHex(encodedMultiHash), expectedMultiHashData);
     }
 
     @Test
     public void add() throws IOException {
+        createFile();
         IPFS ipfs = new IPFS("ipfs.infura.io", 5001, true);
 
-        String encodedHash = ipfs.add("hello.txt");
-
-        System.out.println(encodedHash);
+        String encodedHash = ipfs.add(fileName);
         assertNotNull(encodedHash);
     }
 
     @Test
     public void get() throws IOException {
-        String cid = "Qmd3QDGie6w35bdJvTnrGeH2Ubbg55WqEVMhPk3h7EArkV";
+        String cid = "QmYzW1fXbapdxkZXMQeCYoDCjVc18H8tLfMfrxXRySmQiq";
         IPFS ipfs = new IPFS("ipfs.infura.io", 5001, true);
 
         byte[] content = ipfs.get(cid);
         String data = new String(content);
-        System.out.println(data);
+        assertEquals(text, data);
     }
 }

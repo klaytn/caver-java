e3631a8b74f4b50e54372d801803d4277895041c
kale.kim <kale.kim@groundx.xyz>
2020-10-22 16:16:37 +0900

    add function description.

--- core/src/main/java/com/klaytn/caver/ipfs/IPFS.java
+++ core/src/main/java/com/klaytn/caver/ipfs/IPFS.java
@@ -1,5 +1,22 @@
+/*
+ * Copyright 2020 The caver-java Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the “License”);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.klaytn.caver.ipfs;
 
+import com.klaytn.caver.utils.Utils;
 import io.ipfs.api.MerkleNode;
 import io.ipfs.api.NamedStreamable;
 import io.ipfs.multihash.Multihash;
@@ -8,23 +25,53 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * This class represents uploading and getting file from IPFS.
+ */
 public class IPFS {
+
+    /**
+     * The IPFS instance.
+     */
     io.ipfs.api.IPFS ipfs;
 
+    /**
+     * Creates an IPFS instance.
+     * @param host The host url.
+     * @param port The port number to use.
+     * @param ssl either using ssl or not.
+     */
     public IPFS(String host, int port, boolean ssl) {
         ipfs = new io.ipfs.api.IPFS(host, port, "/api/v0/", ssl);
     }
 
+    /**
+     * Decode encoded multi hash string with Base58 and add hex prefix(0x).
+     * @param encodedHash A encoded string with Base58.
+     * @return String
+     */
     public static String toHex(String encodedHash) {
         Multihash multihash = Multihash.fromBase58(encodedHash);
-        return multihash.toHex();
+        return Utils.addHexPrefix(multihash.toHex());
     }
 
+    /**
+     * Encode hex string with Base58 algorithm.
+     * @param hexString A hex string to encode.
+     * @return String
+     */
     public static String fromHex(String hexString) {
-        Multihash multihash = Multihash.fromHex(hexString);
+        String noPrefixStr = Utils.stripHexPrefix(hexString);
+        Multihash multihash = Multihash.fromHex(noPrefixStr);
         return multihash.toString();
     }
 
+    /**
+     * Add file to IPFS.
+     * @param path A file path to add at IPFS.
+     * @return String
+     * @throws IOException
+     */
     public String add(String path) throws IOException {
         NamedStreamable streamable = new NamedStreamable.FileWrapper(new File(path));
         List<MerkleNode> nodeList = ipfs.add(streamable);
@@ -32,6 +79,12 @@ public class IPFS {
         return nodeList.get(0).hash.toString();
     }
 
+    /**
+     * Get file from IPFS.
+     * @param encodedHash A encoded multi hash string with base58.
+     * @return byte[]
+     * @throws IOException
+     */
     public byte[] get(String encodedHash) throws IOException {
         Multihash multihash = Multihash.fromBase58(encodedHash);
         return this.ipfs.cat(multihash);

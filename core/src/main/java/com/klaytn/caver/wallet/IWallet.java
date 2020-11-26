23a4a5ab4fd7864cf5a9794b10968e21c9a9f24a
kale.kim <kale.kim@groundx.xyz>
2020-11-26 09:56:59 +0900

    modify description

--- core/src/main/java/com/klaytn/caver/wallet/IWallet.java
+++ core/src/main/java/com/klaytn/caver/wallet/IWallet.java
@@ -6,33 +6,36 @@ import com.klaytn.caver.transaction.AbstractTransaction;
 import java.util.List;
 import java.util.function.Function;
 
+/**
+ * This interface manages key data.
+ */
 public interface IWallet {
 
     /**
-     * Generates wallet data.
-     * @param num The number of data to create.
+     * Generates key data to manage in wallet.
+     * @param num The number of key data to create.
      * @return List
      */
     List<String> generate(int num);
 
     /**
-     * Check whether there is a wallet data corresponding to the address passed as a parameter in the wallet.
-     * @param address An address to find wallet data in wallet.
+     * Check whether there is a key data corresponding to the address passed as a parameter in the wallet.
+     * @param address An address to find key data in wallet.
      * @return boolean
      */
     boolean isExisted(String address);
 
     /**
-     * Deletes the wallet data that associates with the given address from wallet.
-     * @param address An address of the wallet data to be deleted in wallet.
+     * Deletes the key data that associates with the given address from wallet.
+     * @param address An address of the key data to be deleted in wallet.
      * @return boolean
      * @throws Exception
      */
     boolean remove(String address) throws Exception;
 
     /**
-     * Signs the transaction using all keys in the wallet data corresponding to the address.
-     * @param address An address of wallet data in wallet.
+     * Signs the transaction using all keys in the key data corresponding to the address.
+     * @param address An address of key data in wallet.
      * @param transaction An AbstractTransaction instance to sign
      * @return AbstractTransaction
      * @throws Exception
@@ -40,8 +43,8 @@ public interface IWallet {
     AbstractTransaction sign(String address, AbstractTransaction transaction) throws Exception;
 
     /**
-     * Signs the FeeDelegatedTransaction using all keys in the wallet data corresponding to the address.
-     * @param address An address of keyring in KeyringContainer.
+     * Signs the FeeDelegatedTransaction using all keys in the key data corresponding to the address.
+     * @param address An address of key data in wallet.
      * @param transaction An AbstractFeeDelegatedTransaction instance to sign.
      * @return AbstractFeeDelegatedTransaction
      * @throws Exception

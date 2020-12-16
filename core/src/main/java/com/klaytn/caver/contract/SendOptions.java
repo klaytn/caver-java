b35e9f34cbc4dd406ac9550b4f282e12769c47d4
kale.kim <kale.kim@groundx.xyz>
2020-12-16 17:57:40 +0900

    add constructor in SendOptions class.

--- core/src/main/java/com/klaytn/caver/contract/SendOptions.java
+++ core/src/main/java/com/klaytn/caver/contract/SendOptions.java
@@ -47,6 +47,17 @@ public class SendOptions {
     public SendOptions() {
 
     }
+
+    /**
+     * Creates a SendOption instance.
+     * It should only be used when executing KIP7 / KIP7 class methods.
+     * Because the KIP7 / KIP7 class method automatically estimates gas.
+     * @param from
+     */
+    public SendOptions(String from) {
+        this(from, (String)null);
+    }
+
     /**
      * Creates a SendOption instance.
      * It sets value to 0x0.

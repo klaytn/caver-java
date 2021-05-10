9b647bc8734e34a0a4b950268d6b7888caa3c156
Denver <aeharvlee@gmail.com>
2021-05-10 13:41:42 +0900

    Change directory structure for wrapper
    This commit is for improving directory structure for wrapper classes.
    
    I’d like to suggest a simple directory structure about wrapper classes.
    Whenever we need a wrapper class,
    how about creating a sub-package called wrapper right under that package?
    
    Examples
    If you want to add wrapper classes for `caver > kct > kip13`,
    all you need to do is just to create directory named “wrapper” under `kip13`.
    Created directory should be: `caver > kct > kip13 > wrapper`.
    
    If you want to add wrapper classes for kct,
    all you need to do is just to create directory named “wrapper” under `kct`.
    Created directory should be: `caver > kct > wrapper`.
    
    Work flow is also simple.
    To add a wrapper class for a package,
    Create a wrapper package under that package and add the wrapper classes to it.
    
    I think this is a simple and intuitive way.

---
+++ core/src/main/java/com/klaytn/caver/account/wrapper/AccountWrapper.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2021 The caver-java Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the “License”);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.klaytn.caver.account.wrapper;
+
+import com.klaytn.caver.account.Account;
+import com.klaytn.caver.account.WeightedMultiSigOptions;
+
+import java.util.List;
+
+/**
+ * Representing an AccountWrapper which wraps all of static methods of Account
+ */
+public class AccountWrapper {
+    public AccountKeyWrapper accountKey;
+
+    /**
+     * Creates an AccountWrapper instance
+     */
+    public AccountWrapper() {
+        accountKey = new AccountKeyWrapper();
+    }
+
+    /**
+     * Create an Account instance that contains the AccountKeyPublic instance
+     *
+     * @param address   The address of Account
+     * @param publicKey public key
+     * @return Account
+     */
+    public Account create(String address, String publicKey) {
+        return Account.createWithAccountKeyPublic(address, publicKey);
+    }
+
+    /**
+     * Create an Account instance that contains the AccountKeyWeightedMultiSig instance
+     * This method set 1 to WeightedMultiSigOptions values(threshold, weights)
+     *
+     * @param address    address of Account
+     * @param publicKeys public key array
+     * @return Account
+     */
+    public Account create(String address, String[] publicKeys) {
+        return Account.createWithAccountKeyWeightedMultiSig(address, publicKeys);
+    }
+
+    /**
+     * Create an Account instance that contains AccountKeyWeightedMultiSig instance
+     *
+     * @param address    address of Account
+     * @param publicKeys public key array
+     * @param options    WeightedMultiSigOptions
+     * @return Account
+     */
+    public Account create(String address, String[] publicKeys, WeightedMultiSigOptions options) {
+        return Account.createWithAccountKeyWeightedMultiSig(address, publicKeys, options);
+    }
+
+    /**
+     * Create an Account instance that contains AccountKeyRoleBased instance
+     * This method set 1 to WeightedMultiSigOptions values(threshold, weights)
+     *
+     * @param address       address of Account
+     * @param publicKeyList List of public key array
+     * @return Account
+     */
+    public Account create(String address, List<String[]> publicKeyList) {
+        return Account.createWithAccountKeyRoleBased(address, publicKeyList);
+    }
+
+    /**
+     * Create an Account instance that contains AccountKeyRoleBased instance
+     *
+     * @param address       address of Account
+     * @param publicKeyList List of public key array
+     * @param optionsList   List of WeightedMultiSigOptions
+     * @return Account
+     */
+    public Account create(String address, List<String[]> publicKeyList, List<WeightedMultiSigOptions> optionsList) {
+        return Account.createWithAccountKeyRoleBased(address, publicKeyList, optionsList);
+    }
+
+    /**
+     * Create an Account instance from RLP-encoded account key
+     *
+     * @param address       address of Account
+     * @param rlpEncodedKey RLP-encoded account key string
+     * @return Account
+     */
+    public Account createFromRLPEncoding(String address, String rlpEncodedKey) {
+        return Account.createFromRLPEncoding(address, rlpEncodedKey);
+    }
+
+    /**
+     * Create an Account instance which has AccountKeyLegacy as an accountKey
+     *
+     * @param address address of Account
+     * @return Account
+     */
+    public Account createWithAccountKeyLegacy(String address) {
+        return Account.createWithAccountKeyLegacy(address);
+    }
+
+    /**
+     * Create an Account instance which has AccountKeyFail as an accountKey
+     *
+     * @param address address of Account
+     * @return Account
+     */
+    public Account createWithAccountKeyFail(String address) {
+        return Account.createWithAccountKeyFail(address);
+    }
+
+    /**
+     * Creates an Account instance which has AccountKeyPublic as an accountKey
+     *
+     * @param address   address of Account
+     * @param publicKey public key
+     * @return Account
+     */
+    public Account createWithAccountKeyPublic(String address, String publicKey) {
+        return Account.createWithAccountKeyPublic(address, publicKey);
+    }
+
+    /**
+     * Create an Account instance which has AccountKeyWeightedMultiSig as an accountKey
+     * This method set 1 to WeightedMultiSigOptions values(threshold, weights)
+     *
+     * @param address    address of Account
+     * @param publicKeys array of public key
+     * @return Account
+     */
+    public Account createWithAccountKeyWeightedMultiSig(String address, String[] publicKeys) {
+        return Account.createWithAccountKeyWeightedMultiSig(address, publicKeys);
+    }
+
+    /**
+     * Create an Account instance which has AccountKeyWeightedMultiSig as an accountKey
+     *
+     * @param address    address of Account
+     * @param publicKeys List of public key array
+     * @param options    List of WeightedMultiSigOptions
+     * @return Account
+     */
+    public Account createWithAccountKeyWeightedMultiSig(String address, String[] publicKeys, WeightedMultiSigOptions options) {
+        return Account.createWithAccountKeyWeightedMultiSig(address, publicKeys, options);
+    }
+
+    /**
+     * Create an Account instance which has AccountKeyRoleBased as an accountKey
+     * This method set 1 to WeightedMultiSigOptions values(threshold, weights)
+     *
+     * @param address            address of Account
+     * @param roleBasedPublicKey List of public key array
+     * @return Account
+     */
+    public Account createWithAccountKeyRoleBased(String address, List<String[]> roleBasedPublicKey) {
+        return Account.createWithAccountKeyRoleBased(address, roleBasedPublicKey);
+    }
+
+    /**
+     * Create an Account instance which has AccountKeyRoleBased as an accountKey
+     *
+     * @param address            address of Account
+     * @param roleBasedPublicKey List of public key array
+     * @param optionsList        List of WeightedMultiSigOptions
+     * @return Account
+     */
+    public Account createWithAccountKeyRoleBased(String address, List<String[]> roleBasedPublicKey, List<WeightedMultiSigOptions> optionsList) {
+        return Account.createWithAccountKeyRoleBased(address, roleBasedPublicKey, optionsList);
+    }
+}
---
+++ core/src/main/java/com/klaytn/caver/transaction/type/wrapper/AccountUpdateWrapper.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2021 The caver-java Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the “License”);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.klaytn.caver.transaction.type.wrapper;
+
+import com.klaytn.caver.rpc.Klay;
+import com.klaytn.caver.transaction.type.AccountUpdate;
+
+/**
+ * Represents an AccountUpdateWrapper
+ * 1. This class wraps all of static methods of AccountUpdate
+ * 2. This class should be accessed via `caver.transaction.accountUpdate
+ */
+public class AccountUpdateWrapper {
+    /**
+     * Klay RPC instance
+     */
+    private Klay klaytnCall;
+
+    /**
+     * Creates an AccountUpdateWrapper instance.
+     * @param klaytnCall Klay RPC instance
+     */
+    public AccountUpdateWrapper(Klay klaytnCall) {
+        this.klaytnCall = klaytnCall;
+    }
+
+    /**
+     * Creates an AccountUpdate instance derived from a RLP-encoded AccountUpdate string.
+     * @param rlpEncoded RLP-encoded AccountUpdate string
+     * @return AccountUpdate
+     */
+    public AccountUpdate create(String rlpEncoded) {
+        AccountUpdate accountUpdate = AccountUpdate.decode(rlpEncoded);
+        accountUpdate.setKlaytnCall(this.klaytnCall);
+        return accountUpdate;
+    }
+
+    /**
+     * Creates an AccountUpdate instance derived from a RLP-encoded AccountUpdate byte array.
+     * @param rlpEncoded RLP-encoded AccountUpdate byte array.
+     * @return AccountUpdate
+     */
+    public AccountUpdate create(byte[] rlpEncoded) {
+        AccountUpdate accountUpdate = AccountUpdate.decode(rlpEncoded);
+        accountUpdate.setKlaytnCall(this.klaytnCall);
+        return accountUpdate;
+    }
+
+    /**
+     * Creates an AccountUpdate instance using AccountUpdate.Builder
+     * @param builder AccountUpdate.Builder
+     * @return AccountUpdate
+     */
+    public AccountUpdate create(AccountUpdate.Builder builder) {
+        return builder
+                .setKlaytnCall(this.klaytnCall)
+                .build();
+    }
+
+    /**
+     * Decodes a RLP-encoded AccountUpdate string.
+     * @param rlpEncoded RLP-encoded AccountUpdate string.
+     * @return AccountUpdate
+     */
+    public AccountUpdate decode(String rlpEncoded) {
+        return AccountUpdate.decode(rlpEncoded);
+    }
+
+    /**
+     * Decodes a RLP-encoded AccountUpdate byte array.
+     * @param rlpEncoded RLP-encoded AccountUpdate byte array.
+     * @return AccountUpdate
+     */
+    public AccountUpdate decode(byte[] rlpEncoded) {
+        return AccountUpdate.decode(rlpEncoded);
+    }
+}

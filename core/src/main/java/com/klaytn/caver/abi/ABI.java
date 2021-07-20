2b75c7e10ede0311b933827b8fa684066b8bbc00
kale.kim <kale.kim@groundx.xyz>
2021-07-20 10:27:31 +0900

    add a example.

--- core/src/main/java/com/klaytn/caver/abi/ABI.java
+++ core/src/main/java/com/klaytn/caver/abi/ABI.java
@@ -385,6 +385,14 @@ public class ABI {
 
     /**
      * Decodes a function call data that composed of function selector and encoded input argument.
+     * <pre>Example :
+     * {@code
+     * String contractABI = "...";
+     * Contract contract = caver.contract.create(contractABI);
+     * ContractMethod method = contract.getMethod("myFunction");
+     *
+     * List<Type> result = ABI.decodeFunctionCall(method, "0x{ABI-encoded string}");
+     * }</pre>
      * @param method The ContractMethod instance to decode ABI-encoded string.
      * @param encodedString The encode function call data string.
      * @return List&lt;Type&gt;
